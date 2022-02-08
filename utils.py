6f6dc2a7deaa313bf7f11e1b133b56f560abdef8
Sanchit Ahuja <sanchit@thelevel.ai>
2022-02-09 01:04:57 +0530

    Add Proto files, server and client

---
+++ utils.py
@@ -0,0 +1,42 @@
+from protos import user_search_pb2
+
+resource_db = [
+    {
+        "user_id": 1,
+        "first_name": "John",
+        "last_name": "Snow",
+        "address": {"city": "NYC", "country": "US"},
+    },
+    {
+        "user_id": 2,
+        "first_name": "Sam",
+        "last_name": "Johnson",
+        "address": {"city": "Mountain View", "country": "US"},
+    },
+    {
+        "user_id": 3,
+        "first_name": "James",
+        "last_name": "Bond",
+        "address": {"city": "NYC", "country": "US"},
+    },
+]
+
+
+def create_message(message):
+    return user_search_pb2.SearchRequest(query=message)
+
+
+def message_iterator(message_list):
+    for message in message_list:
+        yield create_message(message)
+
+
+def get_user(query: str, resource_db: list):
+    for user in resource_db:
+        print("query.lower", query.lower())
+        if (
+                query.lower() in user["first_name"].lower()
+                or query.lower() in user["last_name"].lower()
+        ):
+            print("query.lower", query.lower())
+            yield user
